b9a5f167fae95272ec9560f9e5607f155f81647f
FELIPE SIQUEIRA DOS REIS <88809590+FelipeSking@users.noreply.github.com>
2023-08-23 17:26:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cookieNotification = document.getElementById("cookie-notification");
+    const acceptCookie = document.getElementById("accept-cookie");
+
+    // Verifica se o cookie de aceitação já foi definido
+    if (localStorage.getItem("cookiesAccepted")) {
+        cookieNotification.style.display = "none";
+    }
+
+    acceptCookie.addEventListener("click", function() {
+        // Define um cookie de aceitação com validade de 30 dias
+        const now = new Date();
+        now.setTime(now.getTime() + 30 * 24 * 60 * 60 * 1000);
+        document.cookie = "cookiesAccepted=true; expires=" + now.toUTCString() + "; path=/";
+
+        cookieNotification.style.display = "none";
+    });
+});
